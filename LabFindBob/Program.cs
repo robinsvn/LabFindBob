d49c444abca227cf4adf59bf1d2ab0e33e0a149f
Per A. Bergersen <pab2222@hotmail.com>
2022-09-01 21:54:05 +0200

    Add project files.

---
+++ LabFindBob/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabFindBob
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Oppgave
+            //lag en metode som finner ut hvor mange bob's det er i stringen under.
+            string bob = "obobobobobobobbbooobboboobobobobobobooobbboobooboboboobboodfdfdob4bobbobooob";
+        }
+    }
+}
