96dfc1c2080377bd63becbf5c4a9c66ffc3c5ee7
BuildTools <unconfigured@null.spigotmc.org>
2022-09-26 12:28:24 +0200

    bob

--- LabFindBob/Program.cs
+++ LabFindBob/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace LabFindBob
@@ -13,6 +14,14 @@ namespace LabFindBob
             //Oppgave
             //lag en metode som finner ut hvor mange bob's det er i stringen under.
             string bob = "obobobobobobobbbooobboboobobobobobobooobbboobooboboboobboodfdfdob4bobbobooob";
+            string word = "bob";
+            int count = 0;
+            foreach (Match match in Regex.Matches(bob, word, RegexOptions.IgnoreCase))
+            {
+                count++;
+            }
+            Console.WriteLine("{0}" + " Found " + "{1}" + " Times", word, count);
+            Console.ReadLine();
         }
     }
 }
